cdaef360be32ab734092de60f5de4149b4ad13ce
오인석 <ssoboro1@gmail.com>
2022-06-14 15:51:03 +0900

    start

---
+++ src/test/java/Riot/API/Riot/API/RiotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Riot.API.Riot.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
