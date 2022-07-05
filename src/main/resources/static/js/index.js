37ef35965dd8d885cc2940c7b3acd43356a15bae
오인석 <ssoboro1@gmail.com>
2022-07-05 14:29:14 +0900

    indexpage update

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function characterCheck(obj){
+    let regExp = /[ \{\}\[\]\/?.,;:|\)*~`!^\-_+┼<>@\#$%&\'\"\\\(\=]/;
+    if( regExp.test(obj.value) ){
+        alert("특수문자는 입력하실수 없습니다.");
+        obj.value = obj.value.substring( 0 , obj.value.length - 1 ); // 입력한 특수문자 한자리 지움
+        }
+}
